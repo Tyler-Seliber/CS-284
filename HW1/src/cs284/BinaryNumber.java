274cb448255a9396c7f746efa391b3c8036777ea
Tyler-Seliber <tylerseliber@gmail.com>
2022-01-29 15:49:35 -0500

    Finished HW1, Checking with autograder

--- HW1/src/cs284/BinaryNumber.java
+++ HW1/src/cs284/BinaryNumber.java
@@ -5,54 +5,68 @@ package cs284;
  * I pledge my honor that I have abided by the Stevens Honor System.
  */
 
+import java.util.Arrays;
+
 public class BinaryNumber {
-    private int[] binary;
+    private int[] data;
 
     public BinaryNumber(int length) {
         if (length < 0) {
             illegalArgs("Length must be positive");
         }
-        this.binary = new int[length];
-        for (int i = 0; i < length; i += 1) {
-            binary[i] = 0;
-        }
+        // Create a new array with 'length' zeros
+        this.data = new int[length];
     }
 
     public BinaryNumber(String str) {
         String acceptable_chars = "01";
-        this.binary = new int[str.length()];
+        this.data = new int[str.length()];
         for (int i = 0; i < str.length(); i += 1) {
             char c = str.charAt(str.length() - 1 - i);
             if (!acceptable_chars.contains(c + "")) {
                 illegalArgs("Invalid character in string");
             }
-            binary[i] = Character.getNumericValue(c);
+            data[i] = Character.getNumericValue(c);
         }
-    }
-
-    private static void illegalArgs(String error) {
-        throw new IllegalArgumentException(error);
-    }
-
-    private static void indexOutOfBounds(int index) {
-        throw new IndexOutOfBoundsException("Index: " + index + " is out of bounds");
     }
 
     public int length() {
-        return binary.length;
+        return data.length;
     }
 
     public int getDigit(int index) {
-        if (index < 0 || index >= binary.length) {
+        if (index < 0 || index >= data.length) {
             indexOutOfBounds(index);
         }
-        return binary[index];
+        return data[index];
     }
 
-    public int toInt() {
-        int result = 0;
-        for (int i = 0; i < length(); i += 1) {
-            result += binary[i] * Math.pow(2, i);
+    public static BinaryNumber bwor(BinaryNumber bn1, BinaryNumber bn2) {
+        if (bn1.length() != bn2.length()) {
+            BinaryNumber.illegalArgs("Binary numbers must be of equal length");
+        }
+        BinaryNumber result = new BinaryNumber(bn1.length());
+        for (int i = 0; i < bn1.length(); i += 1) {
+            if (bn1.data[i] == 1 || bn2.data[i] == 1) {
+                result.data[i] = 1;
+            } else {
+                result.data[i] = 0;
+            }
+        }
+        return result;
+    }
+
+    public static BinaryNumber bwand(BinaryNumber bn1, BinaryNumber bn2) {
+        if (bn1.length() != bn2.length()) {
+            BinaryNumber.illegalArgs("Binary numbers must be of equal length");
+        }
+        BinaryNumber result = new BinaryNumber(bn1.length());
+        for (int i = 0; i < bn1.length(); i += 1) {
+            if (bn1.data[i] == 1 && bn2.data[i] == 1) {
+                result.data[i] = 1;
+            } else {
+                result.data[i] = 0;
+            }
         }
         return result;
     }
@@ -68,85 +82,77 @@ public class BinaryNumber {
             illegalArgs("Amount is too large");
         }
 
-        if (direction == -1) {
-            // Left shift
-            extend(amount);
-        } else {
-            // Right shift
-            contract(-1 * amount);
+        if (direction == -1) { // Left shift
+            // Create a new array with contents of this.data followed by additional zeros (become leading zeros in binary number)
+            this.data = Arrays.copyOf(this.data, length() + amount);
+        } else { // Right shift
+            // Create a new array with contents of this.data with first amount elements (least significant bits) removed
+            this.data = Arrays.copyOfRange(this.data, amount, length());
+        }
+    }
+
+    // Binary number addition
+    public void add(BinaryNumber aBinaryNumber) {
+        // If the lengths are different, pad the shorter one with zeros
+        if (this.length() != aBinaryNumber.length()) {
+            BinaryNumber smaller;
+            BinaryNumber larger;
+            if (this.length() > aBinaryNumber.length()) {
+                smaller = aBinaryNumber;
+                larger = this;
+            } else {
+                smaller = this;
+                larger = aBinaryNumber;
+            }
+
+            int[] leadingZeros = new int[larger.length() - smaller.length()];
+            smaller.data = Arrays.copyOf(smaller.data, larger.length());
+
+
+        }
+        int carry = 0;
+        // Add all but the most significant digits
+        for (int i = 0; i < length(); i += 1) {
+            int sum = this.data[i] + aBinaryNumber.data[i] + carry;
+            if (sum >= 2) {
+                carry = 1;
+                this.data[i] = sum - 2; // Subtract 2 to get a binary bit
+            } else {
+                carry = 0;
+                this.data[i] = sum;
+            }
+        }
+
+        // Expand array if there is still a carry
+        if (carry == 1) {
+            this.data = Arrays.copyOf(this.data, length() + 1);
+            this.data[length() - 1] = 1;
         }
     }
 
     public String toString() {
         StringBuilder result = new StringBuilder();
         for (int i = length() - 1; i >= 0; i -= 1) {
-            result.append(binary[i]);
+            result.append(data[i]);
         }
         return result.toString();
     }
 
-    public static BinaryNumber bwor(BinaryNumber bn1, BinaryNumber bn2) {
-        if (bn1.length() != bn2.length()) {
-            BinaryNumber.illegalArgs("Binary numbers must be of equal length");
-        }
-        BinaryNumber result = new BinaryNumber(bn1.length());
-        for (int i = 0; i < bn1.length(); i += 1) {
-            if (bn1.binary[i] == 1 || bn2.binary[i] == 1) {
-                result.binary[i] = 1;
-            } else {
-                result.binary[i] = 0;
-            }
-        }
-        return result;
-    }
-
-    public static BinaryNumber bwand(BinaryNumber bn1, BinaryNumber bn2) {
-        if (bn1.length() != bn2.length()) {
-            BinaryNumber.illegalArgs("Binary numbers must be of equal length");
-        }
-        BinaryNumber result = new BinaryNumber(bn1.length());
-        for (int i = 0; i < bn1.length(); i += 1) {
-            if (bn1.binary[i] == 1 && bn2.binary[i] == 1) {
-                result.binary[i] = 1;
-            } else {
-                result.binary[i] = 0;
-            }
-        }
-        return result;
-    }
-
-    // Method to bitshift left
-    private void extend(int amount) {
-        if (amount < 0) {
-            illegalArgs("Extend amount must be positive");
-        }
-        // Create new array with length = length + amount
-        int[] newBinary = new int[length() + amount];
-        // Fill added indices with 0
-        for (int i = 0; i < amount; i += 1) {
-            newBinary[i] = 0;
-        }
-        // Copy contents of old array into new array
+    public int toInt() {
+        int result = 0;
         for (int i = 0; i < length(); i += 1) {
-            newBinary[amount + i] = binary[i];
+            result += data[i] * Math.pow(2, i);
         }
-        // Set binary to new array
-        binary = newBinary;
+        return result;
     }
 
-    // Method to bitshift right
-    private void contract(int amount) {
-        if (amount > 0) {
-            illegalArgs("Contract amount must be negative");
-        }
-        // Create new array with length = length + amount
-        int[] newBinary = new int[length() + amount];
-        amount *= -1;
-        for (int i = 0; i < amount; i += 1) {
-            newBinary[i] = binary[i + amount];
-        }
-        // Set binary to new array
-        binary = newBinary;
+    // Throw IllegalArgumentException with the given error message
+    private static void illegalArgs(String error) {
+        throw new IllegalArgumentException(error);
     }
 
+    // Throw IndexOutOfBoundsException for the given invalid index
+    private static void indexOutOfBounds(int index) {
+        throw new IndexOutOfBoundsException("Index: " + index + " is out of bounds");
+    }
 }
--- HW1/src/cs284/BinaryNumberTest.java
+++ HW1/src/cs284/BinaryNumberTest.java
@@ -18,6 +18,35 @@ public class BinaryNumberTest {
         p("bwor: " + BinaryNumber.bwor(bn1, bn2));
         p("bwand: " + BinaryNumber.bwand(bn1, bn2));
 
+        BinaryNumber bn3 = new BinaryNumber("01101");
+        BinaryNumber bn4 = new BinaryNumber("01001");
+        bn3.add(bn4);
+        p("01101 + 01001 = " + bn3);
+
+        BinaryNumber bn5 = new BinaryNumber("10110");
+        BinaryNumber bn6 = new BinaryNumber("11101");
+        bn5.add(bn6);
+        p("10110 + 11101 = " + bn5);
+
+        BinaryNumber bn7 = new BinaryNumber("01001");
+        BinaryNumber bn8 = new BinaryNumber("01101");
+        bn7.add(bn8);
+        p("01001 + 01101 = " + bn7);
+
+        BinaryNumber bn9 = new BinaryNumber("11101");
+        BinaryNumber bn10 = new BinaryNumber("10110");
+        bn9.add(bn10);
+        p("11101 + 10110 = " + bn9);
+
+        BinaryNumber bn11 = new BinaryNumber("101");
+        BinaryNumber bn12 = new BinaryNumber("1");
+        bn11.add(bn12);
+        p("101 + 1 = " + bn11);
+
+        BinaryNumber bn13 = new BinaryNumber("101");
+        BinaryNumber bn14 = new BinaryNumber("001");
+        bn13.add(bn14);
+        p("101 + 001 = " + bn13);
     }
 
     public static void p(String s) {
