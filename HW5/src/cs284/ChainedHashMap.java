cd4fe3b086d95b1c5af4302f4e20a673883bb568
Tyler-Seliber <tylerseliber@gmail.com>
2022-04-26 10:12:59 -0400

    Working on HW 5

--- HW5/src/cs284/ChainedHashMap.java
+++ HW5/src/cs284/ChainedHashMap.java
@@ -1,4 +1,62 @@
 package cs284;
 
-public class ChainedHashMap {
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class ChainedHashMap<K, V> implements Map<K, V> {
+
+    private LinkedList<Entry<K, V>>[] buckets;
+
+    public ChainedHashMap() {
+        buckets = new LinkedList[64];
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public int capacity() {
+        return buckets.length;
+    }
+
+    @Override
+    public int rehashes() {
+        return 0;
+    }
+
+    @Override
+    public int[] bucketSizes() {
+        int[] sizes = new int[buckets.length];
+        for (int i = 0; i < buckets.length; i++) {
+            sizes[i] = buckets[i].size();
+        }
+        return sizes;
+    }
+
+    @Override
+    public Object get(Object key) {
+        return null;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return false;
+    }
+
+    @Override
+    public Object put(Object key, Object val) {
+        return null;
+    }
+
+    @Override
+    public Object remove(Object key) {
+        return null;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
 }
